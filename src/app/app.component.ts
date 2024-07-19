b7927a650672ab5a085ddbc20e119e690600222f
br4hm <haehnlein.alexander@gmx.de>
2024-07-19 18:42:13 +0200

    navbar | hero section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LandingPageComponent } from './landingPage/landingPage.component';
+import { NavbarComponent } from './landingPage/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LandingPageComponent, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'sakura';
---
+++ src/app/landingPage/landingPage.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@Component({
+  selector: 'app-landingPage',
+  standalone: true,
+  imports: [NavbarComponent],
+  template: ` <section>
+    <div><app-navbar class="fontRaleway"></app-navbar></div>
+    <h1 class="fontRaleway">SAKURA RAMEN</h1>
+    <h2 class="fontRaleway">BEST RAMEN IN TOWN</h2>
+  </section>`,
+  styleUrls: ['./landingPage.component.scss'],
+})
+export class LandingPageComponent {}
