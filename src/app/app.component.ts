3bdbcdcb8398c1b5c54a9457123060c2998ca524
br4hm <haehnlein.alexander@gmx.de>
2024-07-22 10:14:25 +0200

    try to deploy ftp server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { LandingPageComponent } from './landingPage/landingPage.component';
-import { NavbarComponent } from './landingPage/navbar/navbar.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingPageComponent, NavbarComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MainContentComponent,
+    FooterComponent,
+    ImprintComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
